ba57b1c581210075c265f2c12d6e13223b5b49e7
MH1993 <66976281+HeliumMH@users.noreply.github.com>
2024-04-23 11:21:25 +0200

    Update CIF_Cleaner_for_TOPAS_V5.py

--- CIF_Cleaner_for_TOPAS_V5.py
+++ CIF_Cleaner_for_TOPAS_V5.py
@@ -1,6 +1,6 @@
 """
+Code name: cif_clean_plus2.0
 Author: Meng He (PhD, University of Manchester)
-
 Usage:
 1.remove dummy point related redundant info during TOPAS refinement
 2.check bond distance based on covalent radii
@@ -12,15 +12,20 @@ Usage:
 
 import os
 import re
-from cif_clean_funcs import *
+from funcs import *
+from sort import *
 import subprocess
 import itertools
 
 
+
 intro = 'Enter dir of cif file.\n'
 cif_dir = input(intro).replace('"', '')
-#if '.cif' not in cif_dir:
-#    raise TypeError('not a cif file')
+esd_check = False
+sort_required = True
+
+if '.cif' not in cif_dir:
+    raise TypeError('not a cif file')
 print(cif_dir)
 
 if os.path.split(cif_dir)[0] != '':
@@ -91,7 +96,12 @@ while ln < len(cif_content):
                 print('large esd on distance, check if it is the model or the software issue')
                 ln = ln + 1
                 continue
-
+        else:
+            dist_digits = len(line_content[3].split('.')[1])
+            if dist_digits > 6:
+                print('unrealistic accuracy on bond distance, check if it is the model or the software issue')
+                ln = ln + 1
+                continue
         element_pair.append(re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,5}', line_content[1]).group(1))
         element_pair.append(re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,5}', line_content[2]).group(1))
         print(element_pair[0], ' ', element_pair[1], ' ', line_content[3].split('(')[0])
@@ -112,9 +122,10 @@ while ln < len(cif_content):
             ln = ln + 1
             continue
         elif '(' not in line_content[3]: # skip bond with no esd [EVIL FUNCs]
-            print('missing esd, line skipped')
-            ln = ln + 1
-            continue
+            print('Warning: missing esd')
+            if esd_check == True:
+                ln = ln + 1
+                continue
         else:
             print('okay')
             cif_new_content.append(cif_content[ln])
@@ -126,7 +137,7 @@ while ln < len(cif_content):
     elif re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,5}', line_content[1]) and \
             re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,5}',line_content[2]) and \
             re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,5}',line_content[3]) !=None:
-        print('angle check')
+        #print('angle check')
         redundant=False
 
         # skip large error due to software issue(should not output certain lines at special conditions)
@@ -136,38 +147,38 @@ while ln < len(cif_content):
             error_end=line_content[4].index(')')
             error_digits=error_end-error_start-1
             if error_digits>2:
-                print('large esd on angle, check if it is the model or the software issue, line skipped')
+                #print('large esd on angle, check if it is the model or the software issue, line skipped')
                 ln = ln + 1
                 continue
             elif angle_digits>6:
-                print('unrealistic accuracy on angle, check if it is the model or the software issue, line skipped')
+                #print('unrealistic accuracy on angle, check if it is the model or the software issue, line skipped')
                 ln = ln + 1
                 continue
             elif angle_digits-error_digits<3:
-                print('large esd on angle, check if it is the model or the software issue, line skipped')
+                #print('large esd on angle, check if it is the model or the software issue, line skipped')
                 ln = ln + 1
                 continue
 
         # angle before esd
         if float(line_content[4].split('(')[0]) ==0:
-            print('0 bond angle, line skipped')
+            #print('0 bond angle, line skipped')
             ln = ln + 1
             continue
-        elif float(line_content[4].split('(')[0]) < 90: #remove low angle value
-            print('small angle, line skipped')
+        elif float(line_content[4].split('(')[0]) < 85: #remove low angle value
+            #print('small angle, line skipped')
             ln = ln + 1
             continue
         elif '(' not in line_content[4]: # skip angle with no esd [EVIL FUNCs]
-            print('missing esd, line skipped')
+            #print('missing esd, line skipped')
             ln = ln + 1
             continue
         elif re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,3}',line_content[2]).groups()[0] is 'H' or \
                 re.match(r'^([A-Z][a-z]{0,1})([0-9]{0,4})(\_[0-9]{0,4}){0,3}',line_content[2]).groups()[0] is 'D': #[EVIL FUNCs]
-            print('angle with H in the middle, line skipped')
+            #print('angle with H in the middle, line skipped')
             ln=ln+1
             continue
         elif len(line_content) !=9: #[EVIL FUNCs]
-            print('num of line elements not match with loop_')
+            #print('num of line elements not match with loop_')
             ln=ln+1
             continue
         for pair in itertools.permutations([line_content[1],line_content[2],line_content[3]],2): # check same atom site in angle [EVIL FUNCs]
@@ -175,11 +186,11 @@ while ln < len(cif_content):
                 redundant=True
                 break
         if redundant is True:
-            print('not A-B-C angle, atom sites in angle not unique, line skipped')
+            #print('not A-B-C angle, atom sites in angle not unique, line skipped')
             ln=ln+1
             continue
         else:
-            print('no error, line appended to new cif')
+            #print('no error, line appended to new cif')
             cif_new_content.append(cif_content[ln])
             ln = ln + 1
             continue
@@ -207,5 +218,8 @@ with open(cif_new_dir, 'wt') as f:
 
 #run olex2 directly to check the cleaned cif
 #make sure you have it and 'olex2' is callable from CMD
-
 #subprocess.run([r"C:\Program Files\Olex2-1.5\olex2.exe",os.path.abspath(cif_new_dir)])
+
+#sort things out, after sort you can not run the previous cleaner because the format of cif change slightly
+if sort_required==True:
+    sort(cif_new_dir)
